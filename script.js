d9cbdf2988d699344ff68b8f8390faf04e471187
merinthomas <merinthomas1223@gmail.com>
2023-07-03 12:29:44 -0400

    add initial file

---
+++ script.js
@@ -0,0 +1,50 @@
+const cardsArr = [
+  {
+    name: 'facebook',
+    icon: '<i class="fa-brands fa-facebook"></i>'
+  },
+  {
+    name: 'Google',
+    icon: '<i class="fa-brands fa-google"></i>'
+  },
+  {
+    name: 'codepen',
+    icon: '<i class="fa-brands fa-codepen"></i>'
+  },
+  {
+    name: 'slack',
+    icon: '<i class="fa-brands fa-slack"></i>'
+  },
+  {
+    name: 'indeed',
+    icon: '<i class="fa-brands fa-linkedin"></i>'
+  },
+  {
+    name: 'github',
+    icon: '<i class="fa-brands fa-github"></i>'
+  },
+  {
+    name: 'facebook',
+    icon: '<i class="fa-brands fa-facebook"></i>'
+  },
+  {
+    name: 'Google',
+    icon: '<i class="fa-brands fa-google"></i>'
+  },
+  {
+    name: 'codepen',
+    icon: '<i class="fa-brands fa-codepen"></i>'
+  },
+  {
+    name: 'slack',
+    icon: '<i class="fa-brands fa-slack"></i>'
+  },
+  {
+    name: 'indeed',
+    icon: '<i class="fa-brands fa-linkedin"></i>'
+  },
+  {
+    name: 'github',
+    icon: '<i class="fa-brands fa-github"></i>'
+  }
+]
